a84cad27cde3627cedb274b7548303e486193567
RezaMp <52689106+RezaMpx@users.noreply.github.com>
2021-03-30 19:40:05 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 var a = G('sel'); //ul
 var btn = G('btn'); //add
-var int = 0;
-var user = 0
-var aa = G('span')
-function A() { // Add
-    // if (int > 26) {
-    //     alert('بیشتر از 27 نفر نمیتوانید اضافه کنید')
-    //   return 0;
-    //}
     int++
 var i =10000;
  var iw = i/1000;
